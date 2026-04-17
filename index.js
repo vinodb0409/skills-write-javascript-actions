547f62dc851abb5735e70171c4dd43221bf11ae4
vinodb0409 <vinod.bhasi@ust.com>
2026-04-17 10:35:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,4 @@
-<<<<<<< HEAD
-name: Fetch Dad Jokes
-
-on:
-  workflow_dispatch:
-
-jobs:
-  fetch_jokes:
-    runs-on: ubuntu-latest
-    steps:
-      - name: Checkout repository
-        uses: actions/checkout@v2
-
-      - name: Fetch Dad Jokes
-        id: fetch_jokes
-        run: |
-          npm install axios
-          const axios = require('axios');
-          async function getJoke() {
-            const response = await axios.get('https://icanhazdadjoke.com/', { headers: { 'Accept': 'application/json' } });
-            return response.data.joke;
-          }
-          getJoke().then(joke => {
-            console.log(`::set-output name=joke::${joke}`);
-          });
-=======
-import core from '@actions/core';
+const core = require('@actions/core');
 import axios from 'axios';
 
 async function run() {
@@ -43,4 +17,4 @@ async function run() {
 }
 
 run();
->>>>>>> 1aeb389 (Add ncc build script and bundled dist/index.js)
+
